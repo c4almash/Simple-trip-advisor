6fb85de2bf185f7892dd701169222d645630c742
Shihan Ai <shihan.ai@mail.utoronto.ca>
2014-09-20 05:02:45 -0400

    Finished DirectRoute.java and TrainCompany.java

--- src/main/java/csc301/exercise1/DirectRoute.java
+++ src/main/java/csc301/exercise1/DirectRoute.java
@@ -2,46 +2,85 @@ package csc301.exercise1;
 
 public class DirectRoute {
 	
+	private TrainCompany trainCompany;
+	private String fromStation;
+	private String toStation;
+	private double price;
 	
 	public DirectRoute(TrainCompany trainCompany, String fromStation, String toStation, double price) {
+		setTrainCompany(trainCompany);
+		setFromStation(fromStation);
+		setToStation(toStation);
+		setPrice(price);
 	}
 
 
 	public TrainCompany getTrainCompany() {
-		return null;
+		return this.trainCompany;
 	}
 	
 	public void setTrainCompany(TrainCompany trainCompany) {
+		if (trainCompany == null) {
+			throw new IllegalArgumentException("Train company must not be null");
+		}
+		this.trainCompany = trainCompany;
 	}
 	
 	
 	public String getFromStation() {
-		return null;
+		return this.fromStation;
 	}
 	
 	public void setFromStation(String fromStation) {
+		if (fromStation == null) {
+			throw new IllegalArgumentException("name must not be null");
+		}
+		fromStation = fromStation.trim();
+		if (fromStation.isEmpty()) {
+			throw new IllegalArgumentException("names must contain at least one non-whitespace character");
+		}
+		this.fromStation = fromStation;
 	}
 
 
 	public String getToStation() {
-		return null;
+		return this.toStation;
 	}
 	
 	public void setToStation(String toStation) {
+		if (toStation == null) {
+			throw new IllegalArgumentException("name must not be null");
+		}
+		toStation = toStation.trim();
+		if (toStation.isEmpty()) {
+			throw new IllegalArgumentException("names must contain at least one non-whitespace character");
+		}
+		this.toStation = toStation;
 	}
 
 
 	public double getPrice() {
-		return 0;
+		return this.price;
 	}
 	
 	public void setPrice(double price) {
+		if (price < 0) {
+			throw new IllegalArgumentException("price must be non-negative");
+		}
+		this.price = price;
 	}
 	
 	
 	@Override
 	public boolean equals(Object obj) {
-		return false;
+		//Why is the argument Object type and not DirectRoute type?
+		//Doesn't this function just compare if two DirectRoute objects are equal?
+		//Forces casting.
+		
+		return getTrainCompany().getName() == ((DirectRoute) obj).getTrainCompany().getName() &&
+				getFromStation() == ((DirectRoute) obj).getFromStation() &&
+				getToStation() == ((DirectRoute) obj).getToStation() &&
+				getPrice() == ((DirectRoute) obj).getPrice();
 	}
 	
 	
@@ -50,4 +89,5 @@ public class DirectRoute {
 		return String.format("%s from %s to %s, %.2f$", getTrainCompany().getName(), 
 				getFromStation(), getToStation(), getPrice());
 	}
+
 }
--- src/main/java/csc301/exercise1/MyTripAdvisor.java
+++ src/main/java/csc301/exercise1/MyTripAdvisor.java
@@ -1,17 +1,24 @@
 package csc301.exercise1;
 
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class MyTripAdvisor {
 
+	private List<TrainCompany> trainCompanyList = new ArrayList<TrainCompany>();
 	
 	public MyTripAdvisor() {
 	}
 	
 	
-	public void addTrainCopmany(TrainCompany trainCompany){
-		
+	public void addTrainCompany(TrainCompany trainCompany){
+		//Typo in function name, originally called addTrainCopmany
+		//Same typo in TestMyTripAdvisor file
+		if (trainCompany == null) {
+			throw new IllegalArgumentException("Adding a null company is not allowed");
+		}
+		trainCompanyList.add(trainCompany);
 	}
 	
 	/**
