7b108f8226b51ba4ccc644d2290a8c20ce88a259
timothylai <timothylai_7@hotmail.com>
2014-09-23 21:44:59 -0400

    Added a few test cases to TestTrainCompany

--- src/test/java/csc301/exercise1/TestTrainCompany.java
+++ src/test/java/csc301/exercise1/TestTrainCompany.java
@@ -1,11 +1,16 @@
 package csc301.exercise1;
 
-import static org.junit.Assert.assertNotNull;
-
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
 import org.junit.Test;
+
+import csc301.exercise1.util.Constants;
 
 public class TestTrainCompany {
 
+	private static TrainCompany TC1, TC2, TC3, TC4, TC5,
+		TC6, TC7;	
+	
 	/*
 	 * Test Train Company Names
 	 */
@@ -42,40 +47,64 @@ public class TestTrainCompany {
 		new TrainCompany("E");
 	}
 	
-	public void testtoString(){
-		
-	}
+	@Test
+	public void testGetName(){
+		TC1 = new TrainCompany("TestCompany1");
+		assertEquals("TestCompany1", TC1.getName());
+	}	
 	
+	@Test
 	public void testUpdateDirectRoute(){
+		TC2 = new TrainCompany("TestCompany2");
+		assertEquals(new DirectRoute(TC2, Constants.TORONTO, Constants.OTTAWA, 50), 
+				TC2.createOrUpdateDirectRoute(Constants.TORONTO, Constants.OTTAWA, 50));
+	}
+	
+	public void testDeleteDirectRoute(){
+		
+	}
+		
+	@Test
+	public void testAddGetDirectRoute(){
+		TC3 = new TrainCompany("TestCompany3");
+		TC3.addDirectRoute(Constants.TORONTO, Constants.OTTAWA, 50);
+		assertEquals(new DirectRoute(TC3, Constants.TORONTO, Constants.OTTAWA, 50), 
+				TC3.getDirectRoute(Constants.TORONTO, Constants.OTTAWA));
+	}
+	
+	@Test
+	public void testToString(){
+		TC6 = new TrainCompany("TestCompany6");
+		TC6.addDirectRoute(Constants.TORONTO, Constants.OTTAWA, 50);
+		assertEquals("TestCompany6, offering 1 routes between 2 stations",
+				TC6.toString());
+	}
+	
+	public void testGetDirectRoutesFrom(){
 		
 	}
 	
-	public void testdeleteDirectRoute(){
-		
-	}
-		
-	public void testaddDirectRoute(){
+	public void testGetRoutesTo(){
 		
 	}
 	
-	public void testgetDirectRoute(){
+	public void testGetAllDirectRoutes(){
 		
 	}
 	
-	public void testgetDirectRoutesFrom(){
-		
+	@Test
+	public void testGetDirectRoutesCountEmpty(){
+		TC4 = new TrainCompany("TestCompany4");
+		assertEquals(0, TC4.getDirectRoutesCount());
 	}
 	
-	public void testgetRoutesTo(){
-		
-	}
-	
-	public void testgetAllDirectRoutes(){
-		
-	}
-	
-	public void testgetDirectRoutesCount(){
-		
+	@Test
+	public void testGetDirectRoutesCount(){
+		TC5 = new TrainCompany("TestCompany5");
+		TC5.addDirectRoute(Constants.TORONTO, Constants.OTTAWA, 50);
+		TC5.addDirectRoute(Constants.OTTAWA, Constants.TORONTO, 40);
+		TC5.addDirectRoute(Constants.OTTAWA, Constants.WATERLOO, 60);
+		assertEquals(3, TC5.getDirectRoutesCount());
 	}
 	
 	@Test
