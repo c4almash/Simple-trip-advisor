e05b067e4389841447fad3bb80abff4ba38a129b
Shihan Ai <shihan.ai@mail.utoronto.ca>
2014-09-27 19:27:49 -0400

    Finished MyTripAdvisor

--- src/main/java/csc301/exercise1/MyTripAdvisor.java
+++ src/main/java/csc301/exercise1/MyTripAdvisor.java
@@ -1,14 +1,10 @@
 package csc301.exercise1;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
-
-import csc301.exercise1.util.Utils;
-
 
 public class MyTripAdvisor {
 
@@ -25,6 +21,7 @@ public class MyTripAdvisor {
 		trainCompanyList.add(trainCompany);
 	}
 	
+
 	/**
 	 * Return the price of a cheapest trip from <code>fromStation</code>
 	 * to <code>toStation</code>.
@@ -37,35 +34,18 @@ public class MyTripAdvisor {
 		//Value: (Double) Price to get from "fromStation" to Key
 		HashMap<String, Double> graph = new HashMap<String, Double>();
 		
-		//Stores all the DirectRoute objects from each TrainCompany in MyTripAdvisor
-		Collection<DirectRoute> allRoutes = new ArrayList<DirectRoute>();
-		
-		Iterator<DirectRoute> route;
-		//For each TrainCompany
-		for (int i = 0; i < trainCompanyList.size(); i++) {
-			
-			//Add it's DirectRoutes to allRoutes
-			allRoutes.addAll(trainCompanyList.get(i).getAllDirectRoutes());
-			
-			//Check if it offers a DirectRoute that starts from "fromStation"
-			route = trainCompanyList.get(i).getAllDirectRoutes().iterator();
-			while (route.hasNext()) {
-				if (route.next().getFromStation().equals(fromStation)) {
-					
-					//Found it! Put the cost to get to "fromStation" from "fromStation" as 0.0
-					graph.put(fromStation, 0.0);
-					break;
-				}
-			}
-			
-			//If we didn't find it, return -1
-			if (!graph.containsKey(fromStation)) {
-				return -1;
-			}
+		//Stores all the DirectRoute objects from each TrainCompany in MyTripAdvisor	
+		Collection<DirectRoute> allRoutes = getDirectRoutes(fromStation, toStation);
+		if (invalidFromStation(allRoutes, graph, fromStation)) {
+			//Invalid fromStation, return -1, don't apply dijkstra's algorithm
+			return -1;
 		}
-
+		
+		//Run Dijkstra's algorithm
+		//null :- Don't generate resources used for getCheapestTrip
 		dijkstra(graph, allRoutes, fromStation, toStation, null);
 		
+		//Found a route
 		if (graph.containsKey(toStation)) {
 			return graph.get(toStation);
 		}
@@ -73,7 +53,7 @@ public class MyTripAdvisor {
 			return -1;
 		}
 	}
-
+	
 	/**
 	 * Return a cheapest trip from <code>fromStation</code> to <code>toStation</code>,
 	 * as a list of DirectRoute objects.
@@ -86,39 +66,24 @@ public class MyTripAdvisor {
 		//Value: (Double) Price to get from "fromStation" to Key
 		HashMap<String, Double> graph = new HashMap<String, Double>();
 		
-		//Stores all the DirectRoute objects from each TrainCompany in MyTripAdvisor
-		Collection<DirectRoute> allRoutes = new ArrayList<DirectRoute>();
-		
-		Iterator<DirectRoute> route;
-		//For each TrainCompany
-		for (int i = 0; i < trainCompanyList.size(); i++) {
-			
-			//Add it's DirectRoutes to allRoutes
-			allRoutes.addAll(trainCompanyList.get(i).getAllDirectRoutes());
-			
-			//Check if it offers a DirectRoute that starts from "fromStation"
-			route = trainCompanyList.get(i).getAllDirectRoutes().iterator();
-			while (route.hasNext()) {
-				if (route.next().getFromStation().equals(fromStation)) {
-					
-					//Found it! Put the cost to get to "fromStation" from "fromStation" as 0.0
-					graph.put(fromStation, 0.0);
-					break;
-				}
-			}
-			
-			//If we didn't find it, return -1
-			if (!graph.containsKey(fromStation)) {
-				return null;
-			}
+		//Stores all the DirectRoute objects from each TrainCompany in MyTripAdvisor	
+		Collection<DirectRoute> allRoutes = getDirectRoutes(fromStation, toStation);
+		if (invalidFromStation(allRoutes, graph, fromStation)) {
+			//Invalid fromStation, return null, don't apply dijkstra's algorithm
+			return null;
 		}
+		
+		//Run Dijkstra's algorithm
+		//Generate resources used in cheapestTripHelper and store them in "trip".
+		//"trip" is a List of HashMaps, Lists can be ordered whereas HashMaps cannot. We want to
+		//have an ordered List of HashMaps. This is crucial because we will traverse through this List in order.
+		//These HashMaps maps DirectRoute to graphs (which are implemented as HashMaps)
 		List<HashMap<DirectRoute, HashMap<String, Double>>> trip = new ArrayList<HashMap<DirectRoute, HashMap<String, Double>>>();
 		dijkstra(graph, allRoutes, fromStation, toStation, trip);
-		List<DirectRoute> tripList = new ArrayList<DirectRoute>();
-		
-		
+
 		if (graph.containsKey(toStation)) {
-			helper(trip, graph, fromStation, toStation, tripList);
+			List<DirectRoute> tripList = new ArrayList<DirectRoute>();
+			cheapestTripHelper(trip, graph, fromStation, toStation, tripList);
 			return tripList;
 		}
 		else {
@@ -135,37 +100,49 @@ public class MyTripAdvisor {
 	 */
 	@SuppressWarnings("unchecked")
 	public void dijkstra(HashMap<String, Double> graph, Collection<DirectRoute> allRoutes, String fromStation, String toStation, List<HashMap<DirectRoute, HashMap<String, Double>>> trip) {
+		//Temporary graph used as a clone of graph
 		HashMap<String, Double> tempGraph;
-		Iterator<DirectRoute> route = allRoutes.iterator();
 		List<String> neighbours = new ArrayList<String>();
 		DirectRoute node;
+		Iterator<DirectRoute> route = allRoutes.iterator();
+		//Loops through every DirectRoute
 		while (route.hasNext()) {
 			node = route.next();
+			
+			//Find a station that starts from our "fromStation"
 			if (node.getFromStation().equals(fromStation)) {
+				
+				//Update our graph with the info provided by this DirectRoute
 				if (!graph.containsKey(node.getToStation()) ||
 						graph.get(node.getToStation()) > graph.get(node.getFromStation()) + node.getPrice()) {
 					graph.put(node.getToStation(), graph.get(node.getFromStation()) + node.getPrice());
 				}
+				
+				//Keep track of neighbouring stations
 				if (!neighbours.contains(node.getToStation())) {
 					neighbours.add(node.getToStation());
 				}
 				
+				//After checking this DirectRoute, we remove it from our list of remaining DirectRoutes to check
+				allRoutes.remove(node);
+				route = allRoutes.iterator();
+				
+				//Generate resources used for the cheapestTripHelper function
 				if (trip != null) {
 					HashMap<DirectRoute, HashMap<String, Double>> temp = new HashMap<DirectRoute, HashMap<String, Double>>();
 					tempGraph = (HashMap<String, Double>) graph.clone();
 					temp.put(node, tempGraph);
 					trip.add(temp);
 				}
-				//System.out.println(graph + " ||| " + node);
-				allRoutes.remove(node);
-				route = allRoutes.iterator();
 			}
 		}
+		
 		//Exit condition
 		if (fromStation.equals(toStation) || allRoutes.isEmpty()) {
 			return;
 		}
 		
+		//Recursive call for each neighbouring station
 		if (!neighbours.isEmpty()) {			
 			for (int i = 0; i < neighbours.size(); i++) {
 				dijkstra(graph, allRoutes, neighbours.get(i), toStation, trip);
@@ -173,11 +150,15 @@ public class MyTripAdvisor {
 		}
 	}
 	
-	public void helper(List<HashMap<DirectRoute, HashMap<String, Double>>> trip, HashMap<String, Double> graph, String fromStation, String toStation, List<DirectRoute> tripList) {
+	public void cheapestTripHelper(List<HashMap<DirectRoute, HashMap<String, Double>>> trip, HashMap<String, Double> graph, String fromStation, String toStation, List<DirectRoute> tripList) {
+		//Exit condition
 		if (fromStation.equals(toStation)) {
 			return;
 		}
-		//System.out.println("GRAPH IS " + graph);
+		
+		//Find the first DirectRoute of all our DirectRoutes which has the same toStation as "toStation" argument
+		//AND is the best possible route to get to "toStation".
+		//Set the fromStation of this DirectRoute as the new "toStation" and recurse.
 		for (int i = 0; i < trip.size(); i++) {
 			Iterator<DirectRoute> route = trip.get(i).keySet().iterator();
 			DirectRoute key = route.next();
@@ -185,78 +166,37 @@ public class MyTripAdvisor {
 				tripList.add(0, key);
 				toStation = key.getFromStation();
 				break;
-				
 			}
 		}
-		helper(trip, graph, fromStation, toStation, tripList);
-		//System.out.println(trip);
+		cheapestTripHelper(trip, graph, fromStation, toStation, tripList);
 	}
 	
+	public Collection<DirectRoute> getDirectRoutes(String fromStation, String toStation) {	
+		//Stores all the DirectRoute objects from each TrainCompany in MyTripAdvisor
+		Collection<DirectRoute> allRoutes = new ArrayList<DirectRoute>();
+		
+		//For each TrainCompany
+		for (int i = 0; i < trainCompanyList.size(); i++) {
+			
+			//Add it's DirectRoutes to allRoutes
+			allRoutes.addAll(trainCompanyList.get(i).getAllDirectRoutes());
+		}
+		return allRoutes;
+	}
 	
-	
-	
-	
+	public boolean invalidFromStation(Collection<DirectRoute> allRoutes, HashMap<String, Double> graph, String fromStation) {
+		//Iterator for the allRoutes Collection
+		Iterator<DirectRoute> route = allRoutes.iterator();
 
-	public static void main (String [] args) throws IOException {
-		MyTripAdvisor w = new MyTripAdvisor();
-		TrainCompany e = new TrainCompany("Via");
-		/*e.addRoute("A", "B", 10);
-		e.addRoute("A", "C", 10);
-		e.addRoute("B", "C", 15);
-		e.addRoute("B", "D", 20);
-		e.addRoute("C", "B", 10);
-		e.addRoute("D", "C", 5);*/
-		
-		/*e.addRoute("A", "B", 10);
-		e.addRoute("B", "C", 5);
-		e.addRoute("B", "D", 20);
-		e.addRoute("B", "F", 5);
-		e.addRoute("C", "A", 15);
-		e.addRoute("D", "F", 20);
-		e.addRoute("F", "D", 10);*/
-		
-/*		e.addRoute("A", "B", 10);
-		e.addRoute("B", "D", 20);
-		e.addRoute("B", "F", 5);
-		e.addRoute("F", "D", 10);*/
-		
-/*		e.addRoute("A", "B", 20);
-		e.addRoute("B", "C", 5);
-		e.addRoute("D", "B", 10);*/
-		
-/*		e.addRoute("A", "C", 10);
-		e.addRoute("A", "D", 15);
-		e.addRoute("B", "C", 25);
-		e.addRoute("C", "A", 20);
-		e.addRoute("C", "D", 5);
-		e.addRoute("C", "F", 15);
-		e.addRoute("D", "B", 35);
-		e.addRoute("D", "E", 20);
-		e.addRoute("E", "D", 15);
-		e.addRoute("E", "F", 35);
-		e.addRoute("F", "B", 25);
-		e.addRoute("F", "C", 10);
-		e.addRoute("F", "E", 5);
-		e.addRoute("G", "B", 40);*/
-		
-		w.addTrainCompany(e);
-		
-		TrainCompany r = new TrainCompany("GO");
-		r.addRoute("A", "C", 20);
-		//w.addTrainCompany(r);
-		//System.out.println("The cheapest price is : " + w.getCheapestPrice("B", "D"));
-		//System.out.println("The cheapest trip is : " + w.getCheapestTrip("B", "D"));
-		TrainCompany fastTrain = new TrainCompany("FastTrain");
-		TrainCompany swiftRail = new TrainCompany("SwiftRail");
-		fastTrain.addRoute("Toronto", "Ottawa" ,31);
-		fastTrain.addRoute("Ottawa", "Montreal", 25);
-		swiftRail.addRoute("Toronto", "Ottawa", 30);
-		swiftRail.addRoute("Ottawa", "Montreal", 26);
-		MyTripAdvisor advisor = new MyTripAdvisor();
-		advisor.addTrainCompany(fastTrain);
-		advisor.addTrainCompany(swiftRail);
-		System.out.println(advisor.getCheapestTrip("Toronto", "Montreal"));
-		//System.out.println(fastTrain);
+		while (route.hasNext()) {
+			if (route.next().getFromStation().equals(fromStation)) {
+				
+				//Found it! Put the cost to get to "fromStation" from "fromStation" as 0.0
+				graph.put(fromStation, 0.0);
+				break;
+			}
+		}
+		return !graph.containsKey(fromStation);
 	}
 }
 
